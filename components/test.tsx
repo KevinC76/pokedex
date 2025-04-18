e44c55ebda5a903a9c700295fb86abf8ae04d5af
KevinC76 <xckevinxc67@gmail.com>
2025-04-18 22:03:48 +0700

    first commit

---
+++ components/test.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+
+interface book {
+  name: string;
+  page: number;
+}
+
+type LibraryType = {
+  books: book[];
+};
+
+function Library({ books }: LibraryType) {
+  const [search, setSearch] = useState<string>('');
+
+  const filteredBooks =
+    search === ''
+      ? books
+      : books.filter((curr) =>
+          curr.name.toLowerCase().includes(search.toLowerCase())
+        );
+
+  console.log(books);
+  console.log(search);
+  console.log('ini filter', filteredBooks);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Input search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="border px-3 py-2 rounded-md"
+      />
+
+      {filteredBooks.length > 0 ? (
+        filteredBooks.map((curr) => (
+          <div key={curr.name} className="border rounded p-2">
+            <p className="font-semibold">{curr.name}</p>
+            <p>Halaman: {curr.page}</p>
+          </div>
+        ))
+      ) : (
+        <p className="text-gray-500">Buku tidak ditemukan.</p>
+      )}
+    </div>
+  );
+}
+
+export default Library;
